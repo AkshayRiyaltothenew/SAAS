495474303d643095ec2cf287cb595e9e478f5117
Shefali Sharma <shefali.sharma@tothenew.com>
2017-03-08 16:43:30 +0530

    Chnages : Added comments

--- adding_money.js
+++ adding_money.js
@@ -1,94 +1,83 @@
 var atm=new function() {
+    //Default Denominations
     this.tt_count = 0;
     this.fh_count = 0;
     this.oh_count = 0;
     this.f_count=0;
     this.current_Balance = 0;
     this.max_limit = 25000;
-    this.flag=0;
+    this.flag=0;//used for deposit validation(false initially)
+    
+    //Method for depositing amount in the atm
     this.addMoney = function () {
        if(this.flag)
        {
-
-           $("#error_message").html("*Amount can be deposited only once.");
-
+           alert("Amount can be deposited only once");
            return;
        }
+       //NaN validation
         if(isNaN(document.getElementById('2000_notes').value)||isNaN(document.getElementById('500_notes').value)||isNaN(document.getElementById('100_notes').value)||isNaN(document.getElementById('50_notes').value))
         {
-
-            $("#error_message").html("*Only Integer values can be inserted.");
-
-
+            alert("Please enter only Numbers ");
             return;
         }
-
+        //Check on fields if they are empty
         if(document.getElementById('2000_notes').value.length==0||document.getElementById('500_notes').value.length==0||document.getElementById('100_notes').value.length==0||document.getElementById('50_notes').value.length==0)
        {
-
-           $("#error_message").html("*All fields are mandatory.");
-
+           alert("All fields are required");
            return;
        }
-
+        //negative value validation
         if(document.getElementById('2000_notes').value<0||document.getElementById('500_notes').value<0||document.getElementById('100_notes').value<0||document.getElementById('50_notes').value<0)
         {
-            $("#error_message").html("*Negative Values Not Allowed");
-
-
+            alert("Negative Value Not Allowed");
             return;
         }
 
-                if(document.getElementById('2000_notes').value==0&&document.getElementById('500_notes').value==0&&document.getElementById('100_notes').value==0&&document.getElementById('50_notes').value==0)
+        //Check:Need value for withdrawal
+        if(document.getElementById('2000_notes').value==0&&document.getElementById('500_notes').value==0&&document.getElementById('100_notes').value==0&&document.getElementById('50_notes').value==0)
         {
-            $("#error_message").html("*Enter Some Amount.");
-
+            alert("Enter Some Amount");
             return;
         }
-
-
+        
         this.tt_count = parseInt(document.getElementById('2000_notes').value);
         this.fh_count = parseInt(document.getElementById('500_notes').value);
         this.oh_count = parseInt(document.getElementById('100_notes').value);
         this.f_count = parseInt(document.getElementById('50_notes').value);
         this.current_Balance = (this.tt_count * 2000) + (this.fh_count * 500) + (this.oh_count * 100)+(this.f_count*50);
 
+        //add column on addition of amount
         var markup = "<tr><td style='color:#00A000'>" + this.current_Balance + "</td><td style='color:#00A000'>" + this.tt_count + "</td><td style='color:#00A000'>" + this.fh_count + "</td><td style='color:#00A000'>" + this.oh_count +"</td><td style='color:#00A000'>" + this.f_count+ "</td><td style='color:#00A000'>" + this.current_Balance + "</td></tr>";
         $("table tbody").append(markup);
-        $("#error_message").html("");
         this.flag=1;
-    }
+    };
 
-
-    this.withdraw=function() {
+      //Method for withdrawl of money from the atm
+      this.withdraw=function() {
       var withdraw_amt= parseInt(document.getElementById('amount').value);
-
-        if(document.getElementById('amount').value.length==0)
-        {
-            $("#error_message").html("*Field is mandatory.");
-            return;
-        }
-
+      
+          //current balance check
           if(this.current_Balance==0)
           {
-              $("#error_message").html("*Sorry!! Transaction cannot be processed. Insufficient Balance.");
-
+              alert("Transaction cannnot be completed");
               return;
           }
 
+          //check the max limit before withdrawal
           if(withdraw_amt > this.current_Balance ||withdraw_amt > this.max_limit||withdraw_amt<=0)
           {
-              $("#error_message").html("*Transaction Cannot be Processed.");
-
+              alert("Transaction cannnot be completed");
               return;
           }
-
+          //Withdrawal amount must be multiple of 50 
           if(withdraw_amt%50!=0)
           {
-              $("#error_message").html("*Amount should be in multiple of 50.");
-
+              alert("Amount should be in multiple of 50");
               return ;
           }
+          
+          
         if(withdraw_amt>=2000)
         {
             var wtc=0,wfc=0,whc=0,wfnc=0;
@@ -98,7 +87,7 @@ var atm=new function() {
             {
                 wtc++;
                 wa-=2000;
-               tc--;
+                tc--;
             }
 
             while(fc>0&&wa>=500)
@@ -114,13 +103,13 @@ var atm=new function() {
                 hc--;
 
             }
-             while(fnc>0&&wa>=50) {
+            while(fnc>0&&wa>=50) {
                 wfnc++;
                 wa -= 50;
                 fnc--;
-            }
 
-             var total=(wtc*2000)+(wfc*500)+(whc*100)+(wfnc*50);
+            }
+            var total=(wtc*2000)+(wfc*500)+(whc*100)+(wfnc*50);
 
             if(total==withdraw_amt)
             {
@@ -131,40 +120,36 @@ var atm=new function() {
                 this.current_Balance-=withdraw_amt;
             }
             else{
-                $("#error_message").html("*Transaction Cannot be Processed.");
+                alert("Transaction cannot be processed");
                 return ;
             }
 
         }
-
-
-
-
        else if(withdraw_amt>=500)
         {
             var wtc=0,wfc=0,whc=0,wfnc=0;
             var wa=withdraw_amt, tc=this.tt_count,fc=this.fh_count,hc=this.oh_count,fnc=this.f_count;
-
-
             while(fc>0&&wa>=500)
             {
                 wfc++;
                 wa-=500;
                 fc--;
+
             }
 
             while(hc>0&&wa>=100) {
                 whc++;
                 wa -= 100;
                 hc--;
+
             }
-             while(fnc>0&&wa>=50) {
+            while(fnc>0&&wa>=50) {
                 wfnc++;
                 wa -= 50;
                 fnc--;
-            }
 
-             var total=(wtc*2000)+(wfc*500)+(whc*100)+(wfnc*50);
+            }
+            var total=(wtc*2000)+(wfc*500)+(whc*100)+(wfnc*50);
 
             if(total==withdraw_amt)
             {
@@ -175,14 +160,14 @@ var atm=new function() {
                 this.current_Balance-=withdraw_amt;
             }
             else{
-                $("#error_message").html("*Transaction Cannot be Processed.");
+                alert("Transaction cannot be processed");
                 return ;
             }
 
         }
        else if(withdraw_amt>=100)
         {
-           var wtc=0,wfc=0,whc=0,wfnc=0;
+            var wtc=0,wfc=0,whc=0,wfnc=0;
             var wa=withdraw_amt, tc=this.tt_count,fc=this.fh_count,hc=this.oh_count,fnc=this.f_count;
 
 
@@ -190,13 +175,15 @@ var atm=new function() {
                 whc++;
                 wa -= 100;
                 hc--;
+
             }
             while(fnc>0&&wa>=50) {
                 wfnc++;
                 wa -= 50;
                 fnc--;
             }
-             var total=(wtc*2000)+(wfc*500)+(whc*100)+(wfnc*50);
+        
+            var total=(wtc*2000)+(wfc*500)+(whc*100)+(wfnc*50);
 
             if(total==withdraw_amt)
             {
@@ -207,12 +194,11 @@ var atm=new function() {
                 this.current_Balance-=withdraw_amt;
             }
             else{
-                $("#error_message").html("*Transaction Cannot be Processed.");
+                alert("Transaction cannot be processed");
                 return ;
             }
-
-        }
-        else if(withdraw_amt>=50)
+    }
+    else if(withdraw_amt>=50)
         {
             var wtc=0,wfc=0,whc=0,wfnc=0;
             var wa=withdraw_amt, tc=this.tt_count,fc=this.fh_count,hc=this.oh_count,fnc=this.f_count;
@@ -234,18 +220,20 @@ var atm=new function() {
                 this.current_Balance-=withdraw_amt;
             }
             else{
-                $("#error_message").html("*Transaction Cannot be Processed.");
+                alert("Transaction cannot be processed");
                 return ;
             }
     }
-
+        
    else
         {
-            $("#error_message").html("*Transaction Cannot be Processed.");
+            alert("Transaction cannot be processed");
             return;
         }
-         var markup = "<tr><td style='color: red'>" + withdraw_amt + "</td><td style='color:red'>" + this.tt_count + "</td><td style='color:red'>" + this.fh_count + "</td><td style='color:red'>" + this.oh_count +  "</td><td style='color:red'>" + this.f_count +"</td><td style='color:red'>" + this.current_Balance + "</td></tr>";
+        //add column for each withdrawal
+        var markup = "<tr><td style='color: red'>" + withdraw_amt + "</td><td style='color:red'>" + this.tt_count + "</td><td style='color:red'>" + this.fh_count + "</td><td style='color:red'>" + this.oh_count +  "</td><td style='color:red'>" + this.f_count +"</td><td style='color:red'>" + this.current_Balance + "</td></tr>";
         $("table tbody").append(markup);
-        $("#error_message").html("");
+
     };
 }
+
